5661459fb0613631d54e2018288aa72e4995af03
Juan Carlos <juancarlucci@users.noreply.github.com>
2016-08-31 20:00:01 -0700

    Add getPlacesDetail function and listener for same
    Line 152 for event listener
    Line 277 for function

--- js/script.js
+++ js/script.js
@@ -138,16 +138,19 @@ var KoViewModel = function() {
       map: self.googleMap,
       position: place.latLng,
       animation: google.maps.Animation.DROP,
-      icon: defaultIcon
-      // ,
-      // id: i
+      icon: defaultIcon,
+      title: place.locationName
     };
 
     place.marker = new google.maps.Marker(markerOptions);
 
     // You might also add listeners onto the marker, such as "click" listeners.
+    // place.marker.addListener('click', function() {
+    //   populateInfoWindow(this, largeInfowindow);
+    // });
+    
     place.marker.addListener('click', function() {
-      populateInfoWindow(this, largeInfowindow);
+      getPlacesDetails(this, largeInfowindow);
     });
     // Two event listeners - one for mouseover, one for mouseout,
     // to change the colors back and forth.
@@ -270,6 +273,48 @@ function populateInfoWindow(marker, infowindow) {
     // Open the infowindow on the correct marker.
     infowindow.open(map, marker);
   }
+}
+function getPlacesDetails(marker, infowindow) {
+  var service = new google.maps.places.PlacesService(map);
+  service.getDetails({
+    placeId: marker.id
+  }, function(place, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      // Set the marker property on this infowindow so it isn't created again.
+      infowindow.marker = marker;
+      var innerHTML = '<div>';
+      if (place.name) {
+        innerHTML += '<strong>' + place.name + '</strong>';
+      }
+      if (place.formatted_address) {
+        innerHTML += '<br>' + place.formatted_address;
+      }
+      if (place.formatted_phone_number) {
+        innerHTML += '<br>' + place.formatted_phone_number;
+      }
+      if (place.opening_hours) {
+        innerHTML += '<br><br><strong>Hours:</strong><br>' +
+            place.opening_hours.weekday_text[0] + '<br>' +
+            place.opening_hours.weekday_text[1] + '<br>' +
+            place.opening_hours.weekday_text[2] + '<br>' +
+            place.opening_hours.weekday_text[3] + '<br>' +
+            place.opening_hours.weekday_text[4] + '<br>' +
+            place.opening_hours.weekday_text[5] + '<br>' +
+            place.opening_hours.weekday_text[6];
+      }
+      if (place.photos) {
+        innerHTML += '<br><br><img src="' + place.photos[0].getUrl(
+            {maxHeight: 100, maxWidth: 200}) + '">';
+      }
+      innerHTML += '</div>';
+      infowindow.setContent(innerHTML);
+      infowindow.open(map, marker);
+      // Make sure the marker property is cleared if the infowindow is closed.
+      infowindow.addListener('closeclick', function() {
+        infowindow.marker = null;
+      });
+    }
+  });
 }
 
 ko.applyBindings(new KoViewModel());
